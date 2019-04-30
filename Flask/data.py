5cb52eabc8458bc8380c527fce3c284b1d519f50
Dchole <derekoware47@gmail.com>
2019-04-30 03:28:06 +0100

    first python projects

---
+++ Flask/data.py
@@ -0,0 +1,25 @@
+def Articles():
+    articles = [
+        {
+            'id': 1,
+            'title': 'Article One',
+            'body': 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab, eius iure nobis non ut porro nostrum? Corrupti repudiandae adipisci iusto earum voluptatum sapiente quibusdam dolorem, soluta rem consequuntur a veniam!',
+            'author': 'Derek Oware',
+            'create_date': '04-18-2019'
+        },
+        {
+            'id': 2,
+            'title': 'Article Two',
+            'body': 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab, eius iure nobis non ut porro nostrum? Corrupti repudiandae adipisci iusto earum voluptatum sapiente quibusdam dolorem, soluta rem consequuntur a veniam!',
+            'author': 'John Doe',
+            'create_date': '04-18-2019'
+        },
+        {
+            'id': 3,
+            'title': 'Article Three',
+            'body': 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab, eius iure nobis non ut porro nostrum? Corrupti repudiandae adipisci iusto earum voluptatum sapiente quibusdam dolorem, soluta rem consequuntur a veniam!',
+            'author': 'George Akim',
+            'create_date': '04-18-2019'
+        }
+    ]
+    return articles
